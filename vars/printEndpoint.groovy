df993963c23a92ee4747e88b300ba213aa6185e3
akalinovski <a.kalinovski@gmail.com>
2017-12-08 08:53:41 +0300

    Added steps for jenkins pipelines

---
+++ vars/printEndpoint.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/groovy
+def call(Map config) {
+    def namespace = config.namespace
+    def serviceName = config.serviceName
+    def serviceId = config.serviceId
+    def servicePort = config.port
+
+    try {
+        def serviceEndpoint = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get svc ${serviceId} --no-headers --template '{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}'").trim()
+        echo "${serviceName} deployed at: "
+        print "${serviceName} can be accessed at: http://${serviceEndpoint}:${servicePort}"
+    } catch (err) {
+        echo "Failed to get endpoint"
+        echo "Exception: ${err}"
+        throw err
+    }
+}
