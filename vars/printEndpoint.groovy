d4320e8e721bc4cde737e4a0710d10fd2db2082f
akalinovski <a.kalinovski@gmail.com>
2017-12-11 18:09:44 +0300

    Added missing script file

--- vars/printEndpoint.groovy
+++ vars/printEndpoint.groovy
@@ -7,7 +7,6 @@ def call(Map config) {
 
     try {
         def serviceEndpoint = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get svc ${serviceId} --no-headers --template '{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}'").trim()
-        echo "${serviceName} deployed at: "
         print "${serviceName} can be accessed at: http://${serviceEndpoint}:${servicePort}"
     } catch (err) {
         echo "Failed to get endpoint"
