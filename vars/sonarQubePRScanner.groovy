cfdcee17438a86364d666eafeddefbc792f31514
akalinovski <a.kalinovski@gmail.com>
2017-12-21 18:11:15 +0300

    Changed library

---
+++ vars/sonarQubePRScanner.groovy
@@ -0,0 +1,44 @@
+#!/usr/bin/groovy
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    def serviceName = config.serviceName ?: "sonarqube";
+    def port = config.servicePort ?: "9000";
+    def scannerVersion = config.scannerVersion ?: "2.8"
+    def runSonarScanner = config.runSonarScanner ?: "true"
+
+    if (runSonarScanner) {
+        try {
+            def srcDirectory = pwd();
+            def tmpDir = pwd(tmp: true)
+
+            //work in tmpDir - as sonar scanner will download files from the server
+            dir(tmpDir) {
+                def prId = "${env.JOB_NAME}".tokenize('/').last().tokenize('-').last()
+                def jobName = "${env.JOB_NAME}".tokenize('/')[0]
+                def githubToken = sh(returnStdout: true, script: 'echo $GITHUB_ACCESS_TOKEN').trim()
+
+                def localScanner = "scanner-cli.jar"
+
+                def scannerURL = "http://central.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/${scannerVersion}/sonar-scanner-cli-${scannerVersion}.jar"
+
+                echo "downloading scanner-cli"
+
+                sh "curl -o ${localScanner}  ${scannerURL} "
+
+                echo("executing sonar scanner ")
+
+                sh "java -jar ${localScanner}  -Dsonar.host.url=http://${serviceName}:${port}  -Dsonar.projectKey=${jobName} -Dsonar.sources=${srcDirectory} -Dsonar.github.pullRequest=${prId} -Dsonar.github.oauth=${githubToken} -Dsonar.analysis.mode=preview"
+            }
+
+        } catch (err) {
+            echo "Failed to execute scanner:"
+            echo "Exception: ${err}"
+            throw err;
+        }
+    }
+
+}
